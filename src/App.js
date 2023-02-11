7f609149c7e09d5c8ada406bce169407626509ab
Valentina <110343367+ValentinaPopko@users.noreply.github.com>
2023-02-11 20:57:33 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import { Button, Card } from 'react-bootstrap';
+import Header from './components/header/Header.js';
+import './App.css';
+import Slider from './components/main/Slider.js';
+
+function App() {
+
+    return <div>
+      <Header />
+      <Slider />
+    </div>
+  }
+
+export default App
+
+
